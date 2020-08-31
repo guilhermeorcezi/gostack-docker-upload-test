1cebfe7c2bddcdcd0c5dd431daa4b417b1475d9f
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-08-31 14:34:12 -0300

    showing category data with transactions

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -27,9 +27,9 @@ class Transaction {
   @Column()
   category_id: string;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
   @JoinColumn({ name: 'category_id' })
-  provider: Category;
+  category: Category;
 
   @CreateDateColumn()
   created_at: Date;
